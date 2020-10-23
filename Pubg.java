69b8f52e42b94af615daf94abca47fc0af69e7a5
surakshita <72905080+surakshita@users.noreply.github.com>
2020-10-23 21:56:36 +0530

    Add files via upload

---
+++ Pubg.java
@@ -0,0 +1,12 @@
+public class Pubg
+{
+public static void play()
+{
+System.out.println("called or invoked but main method");
+}
+public static void enemies()
+{
+
+System.out.println("invoked by class pub and by main");
+}
+}
---
+++ Toy.java
@@ -0,0 +1,9 @@
+public class Toy
+{
+public static void main(String[] args)
+{
+System.out.println("Prtinting value from another class");
+Pubg.play();
+Pubg.enemies();
+}
+}
