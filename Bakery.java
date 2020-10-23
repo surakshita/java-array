adbff9f1294e4235b895fc0e28e6cc8baa777df2
surakshita <72905080+surakshita@users.noreply.github.com>
2020-10-23 22:59:24 +0530

    java array program

---
+++ Bakery.java
@@ -0,0 +1,13 @@
+public class Bakery
+{
+public static void itemDetails(String[] items)
+{
+System.out.println("invoked by the main:");
+System.out.println("size:"+items);
+int sizeOfItems=items.length;
+System.out.println("size of Items:"+items);
+System.out.println("elements at index zero:"+items[0]);
+
+
+}
+}
---
+++ Cafe.java
@@ -0,0 +1,8 @@
+public class Cafe
+{
+public static void main(String[] items)
+{
+System.out.println("invoking another class:");
+Bakery.itemDetails(items);
+}
+}
