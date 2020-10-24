87a2fa600205397cab88102950125822853204c8
surakshita <72905080+surakshita@users.noreply.github.com>
2020-10-24 10:06:39 +0530

    Update States.java

--- States.java
+++ States.java
@@ -1,51 +1,21 @@
 public class States
 {
-public static void main(String[] args)
+public static void main(String[] name)
 {
-System.out.println("To display states names:");
-String[] statesInIndia={"karnataks",
-"Assam",
-"Bihar",
-"Goa",
-"Maharastra",
-"Gujarat",
-"Haryana",
-"Kerala",
-"Punjab",
-"Rajasthan",
-"Tamil nadu",
-"Sikkim",
-"telangana",
-"West Bengal",
-"Chhattisgarh"};
-namesOfStates(statesInIndia);
-} 
-public static void namesOfStates(String[] names)
-{
-System.out.println("namesOfStates:"+names);
-
-int size=names.length;
-System.out.println(size);
-System.out.println("Names of states:"+names[0]);
-System.out.println("Names of states:"+names[1]);
-System.out.println("Names of states:"+names[2]);
-System.out.println("Names of states:"+names[3]);
-System.out.println("Names of states:"+names[4]);
-System.out.println("Names of states:"+names[5]);
-System.out.println("Names of states:"+names[6]);
-System.out.println("Names of states:"+names[7]);
-System.out.println("Names of states:"+names[8]);
-System.out.println("Names of states:"+names[9]);
-System.out.println("Names of states:"+names[10]);
-System.out.println("Names of states:"+names[11]);
-System.out.println("Names of states:"+names[12]);
-System.out.println("Names of states:"+names[13]);
-System.out.println("Names of states:"+names[14]);
-
-
+  System.out.println("State name:"+names[0]);
+  System.out.println("State name:"+names[1]);
+  System.out.println("State name:"+names[2]);
+  System.out.println("State name:"+names[3]);
+  System.out.println("State name:"+names[4]);
+  System.out.println("State name:"+names[5]);
+  System.out.println("State name:"+names[6]);
+  System.out.println("State name:"+names[7]);
+  System.out.println("State name:"+names[8]);
+  System.out.println("State name:"+names[9]);
+  System.out.println("State name:"+names[10]);
+  System.out.println("State name:"+names[11]);
+  System.out.println("State name:"+names[12]);
+  System.out.println("State name:"+names[13]);
+  System.out.println("State name:"+names[14]);
 }
 }
-
-
-
-
